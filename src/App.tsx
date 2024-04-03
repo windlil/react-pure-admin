6865c46db1fa7b7de827a1df12867401f7eeeb04
windlil <windlil@outlook.com>
2024-04-03 18:24:13 +0800

    chore: redirect to firstroute

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { ConfigProvider } from "antd";
 import BasicLayout from "@/layout/basic-layout";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import routes from "./router/routes";
 
 const App = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const firstAppChildrenPath = routes[0].children![0].path;
+
+  useEffect(() => {
+    if (pathname === "/") navigate(firstAppChildrenPath as string);
+  }, [pathname, navigate, firstAppChildrenPath]);
   return (
     <>
       <ConfigProvider>
